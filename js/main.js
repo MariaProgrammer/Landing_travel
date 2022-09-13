a5653e9a3b1fe8ab332526df60c7411262f5a87c
Maria <maryfrontend22@gmail.com>
2022-09-13 11:32:29 +0300

    burger menu added

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+// Header menu appearence
+
 console.log('INIT');
 
 (function () {
@@ -9,5 +11,23 @@ console.log('INIT');
 			header.classList.remove('header__active');
 		}
 	}
+
+}());
+
+//Burger handler
+
+(function (){
+	const burgerItem = document.querySelector('.burger');
+	const menu = document.querySelector('.header__nav');
+	const menuCloseItem = document.querySelector('.header__nav-close');
+	
+	burgerItem.addEventListener('click', () => {
+		menu.classList.add('header__nav-active');
+	});
+
+	menuCloseItem.addEventListener('click', () => {
+		menu.classList.remove('header__nav-active');
+	});
+
 
 }());
